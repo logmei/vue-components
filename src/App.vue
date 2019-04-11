962acab38755d8f017fe7b1e4e8c0e718463335f
xiaoyaoyao <dell@x0101015061.corp.ybm100.com>
2019-04-11 12:08:34 +0800

    可编辑表格

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <edit-table></edit-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EditTable from './components/EditTable.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    EditTable
   }
 }
 </script>
---
+++ src/components/EditTable.vue
@@ -0,0 +1,67 @@
+<template>
+    <table class="hovertable">
+        <tr><th v-for="(title,index) in titles" :key="index">{{title}}</th></tr>
+        <tr v-for="(data,index) in datas" :key="index" @click="chooseTr(index)">
+            <td  v-for="([key,value],i) in Object.entries(data)" :key="i" v-show="key!='isEdit'">
+                   <span v-show="!data.isEdit"> {{data[key]}}</span>
+                   <span v-show="data.isEdit"><input type="text"  v-model="data[key]"/></span>
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return {
+            titles:['A','B','C'],
+            datas:[
+                {a:'0',b:'0',c:'0',isEdit:false},
+                {a:'1',b:'1',c:'1',isEdit:false},
+                {a:'2',b:'2',c:'2',isEdit:false},
+            ]
+        }
+    },
+    methods:{
+        /* eslint-disable */
+        chooseTr(index){
+            this.delChoose();
+            this.datas[index].isEdit=true;
+        },
+        delChoose(){//将其他设置为不可编辑
+            for(let data of this.datas){
+                data.isEdit=false;
+            }
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+  .hovertable {
+      width: 100%;
+      font-family: verdana,arial,sans-serif;
+      font-size:11px;
+      color:#333333;
+      border-width: 1px;
+      border-color: #999999;
+      border-collapse: collapse;
+    th {
+        background-color:#c3dde0;
+        border-width: 1px;
+        padding: 8px;
+        border-style: solid;
+        border-color: #a9c6c9;
+    }
+     tr {
+     
+        td {
+        border-width: 1px;
+        padding: 8px;
+        border-style: solid;
+        border-color: #a9c6c9;
+        }
+    }
+ }
+</style>
+
