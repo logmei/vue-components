f704bd1926186343397fdf08d8f2493531a55731
xiaoyaoyao <dell@x0101015061.corp.ybm100.com>
2019-04-12 18:23:16 +0800

    添加菜单

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
+import EditTable from './components/EditTable'
+import Input from './tests/Input'
+
+Vue.use(Router)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
+
+const router = new Router({
+  routes : [
+            {path:'/editTable',name:'editTable',component:EditTable},
+            {path:'/inputClean',name:'inputClean',component:Input}
+          ]
+    })
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
