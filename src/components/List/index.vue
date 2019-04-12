f232e3eeb4835ffb828b7e50e4ca56fd9faea7ef
xiaoyaoyao <dell@x0101015061.corp.ybm100.com>
2019-04-12 18:25:10 +0800

    分页组件

---
+++ src/components/List/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <filter-condition :datas="dataList"></filter-condition>
+</template>
+<script>
+import FilterCondition from './FilterCondition'
+
+export default {
+    data(){
+        return {
+            dataList:[
+                {name:'A1',type:'String'},
+                {name:'A2',type:'String',require:true},
+                {name:'B',type:'Number'},
+                {name:'D',type:'Date'},
+                {name:'E',type:'Select'},
+                {name:'F',type:'checkbox',children:['a','b','c']},
+                {name:'G',type:'between-number'}
+            ]
+        }
+    }
+}
+</script>
