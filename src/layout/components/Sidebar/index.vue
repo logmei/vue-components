6b1e605162c894ad3c1d27ff9db93a401204f7a3
xiaoyaoyao <dell@x0101015061.corp.ybm100.com>
2019-04-12 18:22:40 +0800

    添加菜单

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,38 @@
+<template>
+     <el-menu
+      default-active="2"
+     class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>表单</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">
+            <router-link to="/editTable">可编辑表格</router-link></el-menu-item>
+          <el-menu-item index="1-2">
+            <router-link to="/inputClean">输入框可删除内容</router-link></el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+</template>
+<script>
+/* eslint-disable */
+export default {
+    name:"sidebar",
+    methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
---
+++ src/layout/components/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-row>
+        <el-col :span="6">
+           <sidebar></sidebar>
+        </el-col>
+        <el-col :span="18"><router-view></router-view></el-col>
+    </el-row>
+</template>
+<script>
+import sidebar from './Sidebar'
+export default {
+    components:{sidebar}
+}
+</script>
+
