acac9741d806907424276786afb9d89acb356c67
xiaoyaoyao <dell@x0101015061.corp.ybm100.com>
2019-04-12 18:24:34 +0800

    可删除内容的input组件

---
+++ src/components/InputClean.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-input v-model="input" placeholder="请输入内容" class="clean"><i v-if="showClose" slot="suffix" class="el-input__icon el-icon-circle-close" @click="cleanContent"></i></el-input>
+</template>
+<script>
+/* eslint-disable */
+export default {
+    // props:['value','showClose'],
+    props:{
+        value:{
+            // type:[String,Number,undefined,null,''],
+            required:true
+        },
+        showClose:{
+            type:[Boolean],
+            default:false
+        }
+    },
+    computed:{
+        input:{
+            get:function(){
+                return this.value;
+            },
+            set:function(newValue){
+                this.$emit('input',newValue);
+            }
+        }
+    },
+  
+    methods:{
+        cleanContent(e){
+          this.input=''
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+    .clean{
+       i{
+           cursor: pointer;
+       } 
+    }
+</style>
---
+++ src/tests/Input.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+<input-clean v-model="logmei" :showClose="true"></input-clean> <button @click="logmei=10" >点击设置</button>
+{{logmei}}
+</div>
+    
+    
+</template>
+<script>
+import InputClean from '../components/InputClean'
+
+export default {
+    components:{
+        InputClean
+    },
+    data(){
+       return {
+           logmei:''
+       }
+    }
+}
+</script>
+
